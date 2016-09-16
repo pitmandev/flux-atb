5b4def952355c42ea3ef6ca462e349f1a4d67bc3
pitmadev <66157181@iecisa.corp>
2016-09-16 14:55:41 +0200

    Uso del mósulo de httpdispatcher para implementar un servidor

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+	var dispatcher = require('httpdispatcher');
+	var http = require('http');
+ 
+    dispatcher.setStatic('resources');
+    dispatcher.setStaticDirname('.');
+    
+    dispatcher.onGet("/page1", function(req, res) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Page One');
+    });	
+    
+    dispatcher.onPost("/page2", function(req, res) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Page Two');
+    });
+    
+    dispatcher.beforeFilter(/\//, function(req, res, chain) { //any url 
+        console.log("Before filter");
+        chain.next(req, res, chain);
+    });
+    
+    dispatcher.afterFilter(/\//, function(req, res) { //any url 
+        console.log("After filter");
+        chain.next(req, res, chain);
+    });
+    
+    dispatcher.onError(function(req, res) {
+        res.writeHead(404);
+    });
+    
+    http.createServer(function (req, res) {
+        dispatcher.dispatch(req, res);
+    }).listen(1337, '127.0.0.1');
+    
+    
+    /*
+    GET /page1 => 'Page One'
+    POST /page2 => 'Page Two'
+    GET /page3 => 404
+    GET /resources/images-that-exists.png => Image resource
+    GET /resources/images-that-does-not-exists.png => 404
+    */
